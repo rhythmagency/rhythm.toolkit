7b8a0ed4f086e63a9bfb72ba14bb8d7260b3e21e
Michael Lawrence <michael.lawrence@rhythmagency.com>
2013-12-17 08:07:06 -0800

    Adding error handling.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,284 +1,292 @@
-'use strict';
+try {
+	'use strict';
 
-var _ = require('underscore'),
-	path = require('path-extra');
+	var _ = require('underscore'),
+		path = require('path-extra');
 
-_.str = require('underscore.string');
-_.mixin(_.str.exports());
+	_.str = require('underscore.string');
+	_.mixin(_.str.exports());
 
-module.exports = function (grunt) {
-	// Load all grunt plugins
-	require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+	module.exports = function (grunt) {
+		// Load all grunt plugins
+		require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
-	var target = grunt.option('target'),
-		name = grunt.option('name'),
-		domain = grunt.option('domain'),
-		root,
-		gitOptions = {
-			'stdout': true,
-			'stderr': true,
-			'execOptions': {
-				'cwd': '<%= config.paths.project.root %>'
-			}
-		};
+		var target = grunt.option('target'),
+			name = grunt.option('name'),
+			domain = grunt.option('domain'),
+			root,
+			gitOptions = {
+				'stdout': true,
+				'stderr': true,
+				'execOptions': {
+					'cwd': '<%= config.paths.project.root %>'
+				}
+			};
 
-	if (!target) {
-		grunt.fail.fatal('Target not specified');
-	} else {
-		target = _(target).chain().trim().rtrim('/').rtrim('\\').value();
-
-		if (!grunt.file.exists(target)) {
-			grunt.fail.fatal('Target \"' + target + '\" does not exist.');
-		} else if (!grunt.file.isDir(target)) {
-			grunt.fail.fatal('Target \"' + target + '\" is not a directory.');
+		if (!target) {
+			grunt.fail.fatal('Target not specified');
 		} else {
-			root = target
+			target = _(target).chain().trim().rtrim('/').rtrim('\\').value();
+
+			if (!grunt.file.exists(target)) {
+				grunt.fail.fatal('Target \"' + target + '\" does not exist.');
+			} else if (!grunt.file.isDir(target)) {
+				grunt.fail.fatal('Target \"' + target + '\" is not a directory.');
+			} else {
+				root = target
+			}
 		}
-	}
 
-	if (!name) {
-		grunt.fail.fatal('-name not specified');
-	}
+		if (!name) {
+			grunt.fail.fatal('-name not specified');
+		}
 
-	domain = domain || name; // Fall back to name if no domain specified
+		domain = domain || name; // Fall back to name if no domain specified
 
-	grunt.loadNpmTasks('grunt-file-regex-rename-mod');
-	grunt.loadNpmTasks('grunt-text-replace-mod');
+		grunt.loadNpmTasks('grunt-file-regex-rename-mod');
+		grunt.loadNpmTasks('grunt-text-replace-mod');
 
-	grunt.initConfig({
-		'config': {
-			'name': name,
-			'domain': domain,
-			'data': grunt.file.readJSON(path.join(path.homedir(), '.rhythm.toolkit', 'data.json')),
-			'paths': {
-				'bitbucket': 'https://bitbucket.org/api/2.0/repositories/rhythminteractive/' + name.toLowerCase(),
-				'git': 'ssh://git@bitbucket.org/rhythminteractive/',
-				'temp': path.join(path.tempdir(), 'rhythm.toolkit'),
-				'home': path.join(path.homedir(), '.rhythm.toolkit'),
-				'templates': {
-					'root': 'templates',
-					'docs': 'docs/**',
-					'frontend': 'frontend/**',
-					'umbraco': 'umbraco/**',
-					'gitignore': './templates/git/gitignore.txt'
-				},
-				'project': {
-					'root': root || '.',
-					'gitignore': '<%= config.paths.project.root %>/.gitignore',
-					'trunk': '<%= config.paths.project.root %>/trunk',
-					'frontend': '<%= config.paths.project.trunk %>/<%= config.name %>.Frontend',
-					'website': '<%= config.paths.project.trunk %>/<%= config.name %>.Website',
-					'umbraco': '<%= config.paths.project.website %>/<%= config.name %>.Website',
+		grunt.initConfig({
+			'config': {
+				'name': name,
+				'domain': domain,
+				'data': grunt.file.readJSON(path.join(path.homedir(), '.rhythm.toolkit', 'data.json')),
+				'paths': {
+					'bitbucket': 'https://bitbucket.org/api/2.0/repositories/rhythminteractive/' + name.toLowerCase(),
+					'git': 'ssh://git@bitbucket.org/rhythminteractive/',
+					'temp': path.join(path.tempdir(), 'rhythm.toolkit'),
+					'home': path.join(path.homedir(), '.rhythm.toolkit'),
 					'templates': {
-						'frontend': '<%= config.paths.project.trunk %>/frontend',
-						'umbraco': '<%= config.paths.project.trunk %>/umbraco'
+						'root': 'templates',
+						'docs': 'docs/**',
+						'frontend': 'frontend/**',
+						'umbraco': 'umbraco/**',
+						'gitignore': './templates/git/gitignore.txt'
+					},
+					'project': {
+						'root': root || '.',
+						'gitignore': '<%= config.paths.project.root %>/.gitignore',
+						'trunk': '<%= config.paths.project.root %>/trunk',
+						'frontend': '<%= config.paths.project.trunk %>/<%= config.name %>.Frontend',
+						'website': '<%= config.paths.project.trunk %>/<%= config.name %>.Website',
+						'umbraco': '<%= config.paths.project.website %>/<%= config.name %>.Website',
+						'templates': {
+							'frontend': '<%= config.paths.project.trunk %>/frontend',
+							'umbraco': '<%= config.paths.project.trunk %>/umbraco'
+						}
 					}
-				}
-			},
-			'files': {
-				'data': '<%= config.paths.home %>/data.json',
-				'zip': {
-					'umbraco': '<%= config.paths.temp %>/umbraco.zip'
 				},
-				'remote': {
-					'umbraco': 'http://our.umbraco.org/ReleaseDownload?id=100660'
-				}
-			}
-		},
-
-		'curl': {
-			'umbraco': {
-				'src': '<%= config.files.remote.umbraco %>',
-				'dest': '<%= config.files.zip.umbraco %>'
-			}
-		},
-
-		'copy': {
-			'docs': {
-				'expand': true,
-				'flatten': false,
-				'cwd': '<%= config.paths.templates.root %>',
-				'src': '<%= config.paths.templates.docs %>',
-				'dest': '<%= config.paths.project.root %>'
-			},
-			'frontend': {
-				'expand': true,
-				'flatten': false,
-				'cwd': '<%= config.paths.templates.root %>',
-				'src': '<%= config.paths.templates.frontend %>',
-				'dest': '<%= config.paths.project.trunk %>'
-			},
-			'umbraco': {
-				'expand': true,
-				'flatten': false,
-				'cwd': '<%= config.paths.templates.root %>',
-				'src': '<%= config.paths.templates.umbraco %>',
-				'dest': '<%= config.paths.project.trunk %>'
-			}
-		},
-
-		'rename': {
-			'frontend': {
-				'src': '<%= config.paths.project.templates.frontend %>',
-				'dest': '<%= config.paths.project.frontend %>'
-			},
-			'umbraco': {
-				'src': '<%= config.paths.project.templates.umbraco %>',
-				'dest': '<%= config.paths.project.website %>'
-			}
-		},
-
-		'unzip': {
-			'umbraco': {
-				'files': [
-					{
-						'src': '<%= config.files.zip.umbraco %>',
-						'dest': '<%= config.paths.project.umbraco %>'
-					}
-				]
-			}
-		},
-
-		'fileregexrename-mod': {
-			'build': {
 				'files': {
-					'<%= config.paths.project.trunk %>': '<%= config.paths.project.trunk %>/**'
+					'data': '<%= config.paths.home %>/data.json',
+					'zip': {
+						'umbraco': '<%= config.paths.temp %>/umbraco.zip'
+					},
+					'remote': {
+						'umbraco': 'http://our.umbraco.org/ReleaseDownload?id=100660'
+					}
+				}
+			},
+
+			'curl': {
+				'umbraco': {
+					'src': '<%= config.files.remote.umbraco %>',
+					'dest': '<%= config.files.zip.umbraco %>'
+				}
+			},
+
+			'copy': {
+				'docs': {
+					'expand': true,
+					'flatten': false,
+					'cwd': '<%= config.paths.templates.root %>',
+					'src': '<%= config.paths.templates.docs %>',
+					'dest': '<%= config.paths.project.root %>'
 				},
-				'options': {
-					'replacements': [
+				'frontend': {
+					'expand': true,
+					'flatten': false,
+					'cwd': '<%= config.paths.templates.root %>',
+					'src': '<%= config.paths.templates.frontend %>',
+					'dest': '<%= config.paths.project.trunk %>'
+				},
+				'umbraco': {
+					'expand': true,
+					'flatten': false,
+					'cwd': '<%= config.paths.templates.root %>',
+					'src': '<%= config.paths.templates.umbraco %>',
+					'dest': '<%= config.paths.project.trunk %>'
+				}
+			},
+
+			'rename': {
+				'frontend': {
+					'src': '<%= config.paths.project.templates.frontend %>',
+					'dest': '<%= config.paths.project.frontend %>'
+				},
+				'umbraco': {
+					'src': '<%= config.paths.project.templates.umbraco %>',
+					'dest': '<%= config.paths.project.website %>'
+				}
+			},
+
+			'unzip': {
+				'umbraco': {
+					'files': [
 						{
-							'pattern': 'RHYTHM',
-							'replacement': '<%= config.name %>'
+							'src': '<%= config.files.zip.umbraco %>',
+							'dest': '<%= config.paths.project.umbraco %>'
 						}
 					]
 				}
-			}
-		},
+			},
 
-		'replace-mod': {
-			'build': {
-				'src': ['<%= config.paths.project.trunk %>/**'],
-				'overwrite': true,
-				'replacements': [
-					{
-						'from': 'RHYTHM',
-						'to': '<%= config.name %>'
-					}
-				]
-			}
-		},
-
-		'http': {
-			'create': {
-				'options': {
-					'url': '<%= config.paths.bitbucket %>',
-					'method': 'POST',
-					'auth': {
-						'user': '<%= config.data.user %>',
-						'pass': '<%= config.data.pass %>'
+			'fileregexrename-mod': {
+				'build': {
+					'files': {
+						'<%= config.paths.project.trunk %>': '<%= config.paths.project.trunk %>/**'
 					},
-					'body': 'name=<%= config.domain %>&is_private=1&description=Website%20for%20<%= config.domain %>'
-				}
-			},
-			'delete': {
-				'options': {
-					'url': '<%= config.paths.bitbucket %>',
-					'method': 'DELETE',
-					'auth': {
-						'user': '<%= config.data.user %>',
-						'pass': '<%= config.data.pass %>'
+					'options': {
+						'replacements': [
+							{
+								'pattern': 'RHYTHM',
+								'replacement': '<%= config.name %>'
+							}
+						]
 					}
 				}
-			}
-		},
+			},
 
-		'shell': {
-			'gitclone': {
-				'options': gitOptions,
-				'command': 'git clone <%= config.paths.git %><%= config.name %>.git .'
+			'replace-mod': {
+				'build': {
+					'src': ['<%= config.paths.project.trunk %>/**'],
+					'overwrite': true,
+					'replacements': [
+						{
+							'from': 'RHYTHM',
+							'to': '<%= config.name %>'
+						}
+					]
+				}
 			},
-			'gitremote': {
-				'options': gitOptions,
-				'command': 'git remote add origin <%= config.paths.git %><%= config.name %>.git'
-			},
-			'gitcheckoutmaster': {
-				'options': gitOptions,
-				'command': 'git checkout -b master'
-			},
-			'gitcheckoutdevelopment': {
-				'options': gitOptions,
-				'command': 'git checkout -b development'
-			},
-			'gitcheckoutfrontend': {
-				'options': gitOptions,
-				'command': 'git checkout -b feature/frontend'
-			},
-			'gitadd': {
-				'options': gitOptions,
-				'command': 'git add -A'
-			},
-			'gitcommit': {
-				'options': gitOptions,
-				'command': 'git commit -m "Initial Commit."'
-			},
-			'gitpushmaster': {
-				'options': gitOptions,
-				'command': 'git push origin master'
-			},
-			'gitpushdevelopment': {
-				'options': gitOptions,
-				'command': 'git push origin development'
-			},
-			'gitpushfrontend': {
-				'options': gitOptions,
-				'command': 'git push origin feature/frontend'
-			},
-			'gitmerge': {
-				'options': gitOptions,
-				'command': 'git merge master'
-			},
-			'gitignore': {
-				'options': {
-					'stdout': true,
-					'stderr': false
-				},
-				'command': 'cp "<%= config.paths.templates.gitignore %>" "<%= config.paths.project.gitignore %>"'
-			},
-			'npminstall': {
-				'options': {
-					'stdout': true,
-					'stderr': false,
-					'execOptions': {
-						'cwd': '<%= config.paths.project.frontend %>'
+
+			'http': {
+				'create': {
+					'options': {
+						'url': '<%= config.paths.bitbucket %>',
+						'method': 'POST',
+						'auth': {
+							'user': '<%= config.data.user %>',
+							'pass': '<%= config.data.pass %>'
+						},
+						'body': 'name=<%= config.domain %>&is_private=1&description=Website%20for%20<%= config.domain %>'
 					}
 				},
-				'command': 'npm install'
+				'delete': {
+					'options': {
+						'url': '<%= config.paths.bitbucket %>',
+						'method': 'DELETE',
+						'auth': {
+							'user': '<%= config.data.user %>',
+							'pass': '<%= config.data.pass %>'
+						}
+					}
+				}
+			},
+
+			'shell': {
+				'gitclone': {
+					'options': gitOptions,
+					'command': 'git clone <%= config.paths.git %><%= config.name %>.git .'
+				},
+				'gitremote': {
+					'options': gitOptions,
+					'command': 'git remote add origin <%= config.paths.git %><%= config.name %>.git'
+				},
+				'gitcheckoutmaster': {
+					'options': gitOptions,
+					'command': 'git checkout -b master'
+				},
+				'gitcheckoutdevelopment': {
+					'options': gitOptions,
+					'command': 'git checkout -b development'
+				},
+				'gitcheckoutfrontend': {
+					'options': gitOptions,
+					'command': 'git checkout -b feature/frontend'
+				},
+				'gitadd': {
+					'options': gitOptions,
+					'command': 'git add -A'
+				},
+				'gitcommit': {
+					'options': gitOptions,
+					'command': 'git commit -m "Initial Commit."'
+				},
+				'gitpushmaster': {
+					'options': gitOptions,
+					'command': 'git push origin master'
+				},
+				'gitpushdevelopment': {
+					'options': gitOptions,
+					'command': 'git push origin development'
+				},
+				'gitpushfrontend': {
+					'options': gitOptions,
+					'command': 'git push origin feature/frontend'
+				},
+				'gitmerge': {
+					'options': gitOptions,
+					'command': 'git merge master'
+				},
+				'gitignore': {
+					'options': {
+						'stdout': true,
+						'stderr': false
+					},
+					'command': 'cp "<%= config.paths.templates.gitignore %>" "<%= config.paths.project.gitignore %>"'
+				},
+				'npminstall': {
+					'options': {
+						'stdout': true,
+						'stderr': false,
+						'execOptions': {
+							'cwd': '<%= config.paths.project.frontend %>'
+						}
+					},
+					'command': 'npm install'
+				}
+			},
+
+			'git': {
+				'init': ['shell:gitclone', 'shell:gitcheckoutmaster'],
+				'push': ['shell:gitadd', 'shell:gitcommit', 'shell:gitcheckoutdevelopment', 'shell:gitmerge', 'shell:gitcheckoutfrontend', 'shell:gitmerge', 'shell:gitpushmaster', 'shell:gitpushdevelopment', 'shell:gitpushfrontend']
 			}
-		},
+		});
 
-		'git': {
-			'init': ['shell:gitclone', 'shell:gitcheckoutmaster'],
-			'push': ['shell:gitadd', 'shell:gitcommit', 'shell:gitcheckoutdevelopment', 'shell:gitmerge', 'shell:gitcheckoutfrontend', 'shell:gitmerge', 'shell:gitpushmaster', 'shell:gitpushdevelopment', 'shell:gitpushfrontend']
-		}
+
+		grunt.registerTask('default', ['login']);
+
+		grunt.task.registerMultiTask('git', 'Git Tasks', function () {
+			grunt.task.run(this.data);
+		});
+
+		grunt.task.registerTask('login', 'Login to BitBucket', function (user, pass) {
+			var file = grunt.config.get('config.files.data'),
+				data = {
+					'user': user,
+					'pass': pass
+				};
+
+			grunt.file.write(file, JSON.stringify(data));
+		});
+
+		grunt.registerTask('umbraco', ['http:create', 'git:init', 'shell:gitignore', 'copy:docs', 'copy:frontend', 'copy:umbraco', 'fileregexrename-mod', 'replace-mod', 'rename', 'shell:npminstall', 'curl:umbraco', 'unzip:umbraco', 'git:push']);
+		grunt.registerTask('frontend', ['http:create', 'git:init', 'shell:gitignore', 'copy:docs', 'copy:frontend', 'fileregexrename-mod', 'replace-mod', 'rename:frontend', 'shell:npminstall', 'git:push']);
+	};
+
+	process.on('uncaughtException', function (err) {
+		console.error(err.stack);
 	});
-
-
-	grunt.registerTask('default', ['login']);
-
-	grunt.task.registerMultiTask('git', 'Git Tasks', function () {
-		grunt.task.run(this.data);
-	});
-
-	grunt.task.registerTask('login', 'Login to BitBucket', function (user, pass) {
-		var file = grunt.config.get('config.files.data'),
-			data = {
-				'user': user,
-				'pass': pass
-			};
-
-		grunt.file.write(file, JSON.stringify(data));
-	});
-
-	grunt.registerTask('umbraco', ['http:create', 'git:init', 'shell:gitignore', 'copy:docs', 'copy:frontend', 'copy:umbraco', 'fileregexrename-mod', 'replace-mod', 'rename', 'shell:npminstall', 'curl:umbraco', 'unzip:umbraco', 'git:push']);
-	grunt.registerTask('frontend', ['http:create', 'git:init', 'shell:gitignore', 'copy:docs', 'copy:frontend', 'fileregexrename-mod', 'replace-mod', 'rename:frontend', 'shell:npminstall', 'git:push']);
-};
+} catch (err) {
+	console.error(err);
+}
